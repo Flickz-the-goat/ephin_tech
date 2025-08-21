9492dae6f859f6f33d901740f31547a2a5e62e99
Anirudh Sharma <aniaug12@gmail.com>
2025-08-21 14:39:54 -0400

    meta data added

--- my-app/sections/ExtraInfo.tsx
+++ my-app/sections/ExtraInfo.tsx
@@ -1,16 +1,30 @@
 export default function ExtraInfo() {
-    return (
-        <section className="min-w-screen px-5 mt-6 bg-[#FEFFF8] text-[#1E1E1E] ">
-            <div className="mx-auto max-w-6xl bg-[#1e1e1e] text-[#FEFFF8] py-4 px-2 md:px-8 flex flex-col lg:flex-row gap-4 lg:text-left text-center">
-                <div className="space-y-4 flex-1">
-                    <h1 className="text-5xl lg:text-7xl font-extrabold">BUILD<br/>WHAT&apos;S<br/>NEXT</h1>
-                    <p className="italic font-light text-md lg:text-lg">We automate your business, so you can focus on what matters</p>
-                </div>
+  return (
+    <section className="min-w-screen px-5 mt-6 bg-[#FEFFF8] text-[#1E1E1E] ">
+      <div className="mx-auto max-w-6xl bg-[#1e1e1e] text-[#FEFFF8] py-4 px-2 md:px-8 flex flex-col lg:flex-row gap-4 lg:text-left text-center">
+        <div className="space-y-4 flex-1">
+          <h1 className="text-5xl lg:text-7xl font-extrabold">
+            BUILD
+            <br />
+            WHAT&apos;S
+            <br />
+            NEXT
+          </h1>
+          <p className="italic font-light text-md lg:text-lg">
+            We automate your business, so you can focus on what matters
+          </p>
+        </div>
 
-                <div className="h-[300px] overflow-hidden flex-1 border border-[#fefff8] rounded-lg">
-                    <video src={"/videos/info-video.mp4"} autoPlay muted loop className="object-contain"></video>
-                </div>
-            </div>
-        </section>
-    );
+        <div className="h-[300px] overflow-hidden flex-1 border border-[#fefff8] rounded-lg">
+          <video
+            src={"/videos/info-video.mp4"}
+            autoPlay
+            muted
+            loop
+            className="object-contain"
+          ></video>
+        </div>
+      </div>
+    </section>
+  );
 }
--- my-app/sections/Model.tsx
+++ my-app/sections/Model.tsx
@@ -1,19 +1,40 @@
 import ModelCard from "@/components/ModelCard";
 import SectionWrapper from "@/components/SectionWrapper";
 
-export default function Model(){
-    return(
+export default function Model() {
+  return (
     <SectionWrapper className={" bg-[#FEFFF8] text-[#1E1E1E] "}>
-            <div id="model" className="mx-auto max-w-7xl px-5 space-y-8 flex flex-col items-center">
-                <h1 className="text-4xl md:text-6xl lg:text-7xl font-extrabold text-center">The <span className="text-[#86bc25] underline">CARE</span> Model</h1>
+      <div
+        id="model"
+        className="mx-auto max-w-7xl px-5 space-y-8 flex flex-col items-center"
+      >
+        <h1 className="text-4xl md:text-6xl lg:text-7xl font-extrabold text-center">
+          The <span className="text-[#86bc25] underline">CARE</span> Model
+        </h1>
 
-                <div className="space-y-4">
-                    <ModelCard video_src="/videos/capture-video.mp4" title="C - Capture" desc="Have a personal AI secretary to answer and book appointments 24/7."/>
-                    <ModelCard video_src="/videos/automate-video.mp4" title="A - Automate" desc="Let our AI agents do your boring tasks."/>
-                    <ModelCard video_src="/videos/retain-video.mp4" title="R - Retain" desc="Personalize recalls and reminders to reduce no‑shows and boost loyalty."/>
-                    <ModelCard video_src="/videos/elevate-video.mp4" title="E - Elevate" desc="Turn practice data into insights that improve chair utilization and revenue."/>
-                </div>
-            </div>
+        <div className="space-y-4">
+          <ModelCard
+            video_src="/videos/capture-video.mp4"
+            title="C - Capture"
+            desc="Have a personal AI secretary to answer and book appointments 24/7."
+          />
+          <ModelCard
+            video_src="/videos/automate-video.mp4"
+            title="A - Automate"
+            desc="Let our AI agents do your boring tasks."
+          />
+          <ModelCard
+            video_src="/videos/retain-video.mp4"
+            title="R - Retain"
+            desc="Personalize recalls and reminders to reduce no‑shows and boost loyalty."
+          />
+          <ModelCard
+            video_src="/videos/elevate-video.mp4"
+            title="E - Elevate"
+            desc="Turn practice data into insights that improve chair utilization and revenue."
+          />
+        </div>
+      </div>
     </SectionWrapper>
-    )
+  );
 }
